fa201029c73486ee83fceec759ffbe260dcf3218
복혜원 <pokhyewon@Mac-Studio.local>
2025-05-29 15:34:20 +0900

    fix: wrap main content in container for proper padding

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,7 +10,7 @@ type Props = {
 export default function Layout({ children }: Props) {
   const { pathname } = useRouter()
 
-  // 홈('/') 경로인 경우 body에 'home' 클래스를 붙여서 globals.css에서 background를 분기 처리
+  // 홈('/') 경로인 경우 body에 'home' 클래스를 붙여서 globals.css에서 background 분기 처리
   React.useEffect(() => {
     if (typeof document !== 'undefined') {
       const cls = 'home'
@@ -19,11 +19,15 @@ export default function Layout({ children }: Props) {
   }, [pathname])
 
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main style={{ paddingTop: '0px' }}>
-        {children}
+
+      {/* 이 부분만(content area) 컨테이너, 패딩이 적용됩니다 */}
+      <main className="flex-grow">
+        <div className="container mx-auto px-4 py-6">
+          {children}
+        </div>
       </main>
-    </>
-  );
+    </div>
+  )
 }
